ed1355700228f568884fc444c0709223803376f9
Beysim Mustafov <48449800+bsm3@users.noreply.github.com>
2023-05-08 13:55:06 +0300

    Add files via upload

---
+++ matrix_rain/matrix.h
@@ -0,0 +1,19 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef struct matrix
+{
+    int row, i[3], x[6], y, ch;
+    int i1;
+    char * arr;
+    char * in[30];
+
+}matrix;
+
+
+void gotoxy(int, int);
+void pr(matrix *, int);
+void update(matrix *, int);
+void size(matrix *, int);
+
+#endif // MATRIX_H
